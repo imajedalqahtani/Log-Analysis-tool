dbd7f0b040c24268bb4fa6344f413f4c1e02e719
imajedalqahtani <imajed.alqahtani@gmail.com>
2026-05-05 02:22:58 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,17 +1,17 @@
-import re  # Used for pattern matching
+import re  # used for pattern matching
 from collections import defaultdict  # dictionary that auto-initializes values
 
 
 # function to analyze log file
 def analyze_logs(file_path):
 
-    # Dictionary to store failed login counts per IP
+    # dictionary to store failed login counts per IP
     failed_logins = defaultdict(int)
 
     # open the log file in read mode
     with open(file_path, 'r') as file:
 
-        # Read file line by line
+        # read file line by line
         for line in file:
 
             # convert line to lowercase and check for "failed"
@@ -20,11 +20,11 @@ def analyze_logs(file_path):
                 # extract IP address using regex pattern
                 ip_match = re.search(r'\d+\.\d+\.\d+\.\d+', line)
 
-                # If IP is found
+                # if IP is found
                 if ip_match:
-                    ip = ip_match.group()  # Get actual IP string
+                    ip = ip_match.group()  # get actual IP string
 
-                    # Increase count for this IP
+                    # increase count for this IP
                     failed_logins[ip] += 1
 
     print("\nSuspicious Activity Report:")
@@ -32,7 +32,7 @@ def analyze_logs(file_path):
     # loop through all recorded IPs
     for ip, count in failed_logins.items():
 
-        # If more than 3 failed attempts then suspicious
+        # if more than 3 failed attempts then suspicious
         if count > 3:
             print(f"IP {ip} → {count} failed attempts (SUSPICIOUS)")
 
@@ -40,5 +40,5 @@ def analyze_logs(file_path):
 # ask user for log file path
 log_file = input("Enter log file path: ")
 
-# Run analysis
+# run analysis
 analyze_logs(log_file)
